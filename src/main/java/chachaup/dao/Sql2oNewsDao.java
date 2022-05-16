576bc4a8184debe1f174ee3cfa5a00f0966280ea
chacha <chachaerickjo@gmail.com>
2022-05-16 16:27:17 +0300

    Add logic for adding news, deleting by Id, and reading from the db

--- src/main/java/chachaup/dao/Sql2oNewsDao.java
+++ src/main/java/chachaup/dao/Sql2oNewsDao.java
@@ -1,4 +1,76 @@
 package chachaup.dao;
 
-public class Sql2oNewsDao {
+import chachaup.domain.News;
+import chachaup.interfaces.NewsDao;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+import java.util.List;
+
+public class Sql2oNewsDao implements NewsDao {
+    private final Sql2o sql2o;
+
+    public Sql2oNewsDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(News news) {
+        String sql = "INSERT INTO news (content, employeeid, dateCreated, published) VALUES (:content,:employee,:dateCreated, :published)";
+        try(Connection con = sql2o.open()){
+            int id = (int) con.createQuery(sql,true)
+                    .bind(news)
+                    .executeUpdate()
+                    .getKey();
+            news.setId(id);
+        } catch (Sql2oException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    @Override
+    public News findById(int id) {
+        String sql = "SELECT * FROM news WHERE id = :id";
+        try(Connection con = sql2o.open()){
+            News news = con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeAndFetchFirst(News.class);
+            return news;
+        }
+    }
+
+    @Override
+    public List<News> getAll() {
+        String sql = "SELECT * FROM news";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public List<News> findByAuthor(int employeeId) {
+        String sql = "SELECT * FROM news WHERE employeeid = :employeeId";
+        try(Connection con = sql2o.open()){
+            return con.createQuery(sql)
+                    .addParameter("employeeId",employeeId)
+                    .throwOnMappingFailure(false)
+                    .executeAndFetch(News.class);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE FROM news WHERE id = :id";
+        try(Connection con = sql2o.open()){
+            con.createQuery(sql)
+                    .addParameter("id",id)
+                    .executeUpdate();
+        } catch (Sql2oException ex){
+            System.out.println(ex.getMessage());
+        }
+
+    }
 }
