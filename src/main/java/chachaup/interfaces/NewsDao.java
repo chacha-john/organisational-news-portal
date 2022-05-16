b4655ae486810d0a017cc5365a5092ccb7c4b701
chacha <chachaerickjo@gmail.com>
2022-05-16 15:13:25 +0300

    Add interface for News

--- src/main/java/chachaup/interfaces/NewsDao.java
+++ src/main/java/chachaup/interfaces/NewsDao.java
@@ -1,4 +1,21 @@
 package chachaup.interfaces;
 
+import chachaup.domain.News;
+
+import java.util.List;
+
 public interface NewsDao {
+    //create
+    void add(News news);
+
+    //read
+    News findById(int id);
+    List<News> getAll();
+    List<News> findByAuthor(int employeeId);
+
+
+    //update
+
+    //delete
+    void deleteById(int id);
 }
---
+++ src/main/resources/DB/create.sql
@@ -0,0 +1,18 @@
+SET MODE postgreSQL;
+
+CREATE TABLE IF NOT EXISTS users (
+id int PRIMARY KEY auto_increment,
+name VARCHAR,
+phone VARCHAR,
+address VARCHAR,
+email VARCHAR,
+position VARCHAR,
+role VARCHAR,
+departmentId int
+);
+
+CREATE TABLE IF NOT EXISTS departments(
+id int PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR,
+description VARCHAR
+);
