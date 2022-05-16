4d3c9c1b33e5c2f8c7eade666835287132e3f10f
chacha <chachaerickjo@gmail.com>
2022-05-16 16:25:01 +0300

    Add query to create table for the news if the table does not exist

--- src/main/resources/DB/create.sql
+++ src/main/resources/DB/create.sql
@@ -16,3 +16,11 @@ id int PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR,
 description VARCHAR
 );
+
+CREATE TABLE IF NOT EXISTS news(
+id int PRIMARY KEY auto_increment,
+content VARCHAR,
+employeeid int,
+datecreated Date,
+published varchar
+);
