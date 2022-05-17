51181e809e6be14d49dcc44fb54897d2ac7b199b
chacha <chachaerickjo@gmail.com>
2022-05-17 08:45:55 +0300

    FT - Modify findByAuthor method to return a list of news

--- src/main/java/chachaup/interfaces/DepartmentNewsDao.java
+++ src/main/java/chachaup/interfaces/DepartmentNewsDao.java
@@ -1,4 +1,22 @@
 package chachaup.interfaces;
 
+import chachaup.domain.DepartmentNews;
+import chachaup.domain.News;
+
+import java.util.List;
+
 public interface DepartmentNewsDao {
+    //create
+    void add(DepartmentNews news);
+
+    //read
+    News findById(int id);
+    List<DepartmentNews> all();
+    List<DepartmentNews> findByNewsAuthor(int employeeId);
+
+
+    //update
+
+    //delete
+    void deleteById(int id);
 }
